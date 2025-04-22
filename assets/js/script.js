59c0f8501c24c1088b43f97e585bd940ba97c52c
Natthapat Daphan <65160163@go.buu.ac.th>
2025-04-23 00:33:34 +0700

    showAndHideHambuergerIcon

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@
 function toggleHamburgerIcon(x) {
   x.classList.toggle("change");
 }
+
+
+let hamburgerIcon = document.getElementById("hamburgerIcon")
+ if(hamburgerIcon){
+    hamburgerIcon.addEventListener("click",function(){
+        let mobileMenu = document.getElementById("mobile-Menu");
+        if(mobileMenu.style.display=== "block"){
+            mobileMenu.style.display="none"
+
+        }else{
+            mobileMenu.style.display ="block"
+        }
+    })
+ }
